ae4902d52b65d246f33705ea38a271254f5d64fd
Rohit Sharma <rohitsharma@gmail.com>
2026-01-19 15:31:47 +0530

    up

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
    server: {
     proxy: {
-      '/api': 'http://localhost:9000',
+      '/api': 'https://leave-management-full-stack-backend.onrender.com',
     },
   },
   resolve: {
